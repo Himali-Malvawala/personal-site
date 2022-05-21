d59ff7ef215874baf1b4777604c3f1607641ce89
Himali Malvawala <himalimalvawala@gmail.com>
2022-05-21 10:14:45 +0530

    update about page

--- pages/about.js
+++ pages/about.js
@@ -8,31 +8,35 @@ const About = () => {
         <p className="text-xl text-gray-700 pb-9">
           Hey!! My name is Himali Malvawala and I'm a web developer. I learned
           Web Development recently, so I guess I can call myself a beginner and
-          to really get into that coding mindset I keep creating websites and
-          I'm also learning new things related to that.
+          as a beginner to really get into that coding mindset I keep creating
+          websites and keep learning new things.
         </p>
 
         <p className="text-xl text-gray-700 pb-9">
           So How it all started?. Well, I have a degree in Business
-          Administration field and while I was pursuing that, around end of that
-          I started learning Web Development.
+          Administration field and while I was pursuing that, my brother
+          encouraged me to start learning Web Development. And after couple of
+          months I decided that I want to have a career in this field, so here
+          we are!!
         </p>
 
         <p className="text-xl text-gray-700 pb-9">
-          About my interests, I like to listen to music while doing work and
-          while coding I like to listen{"   "}
+          About my interests, I like to listen to music while coding, and
+          usually I listen to{"   "}
           <span>
             <a
+              target={"_blank"}
               className="underline hover:text-blue"
               href="https://open.spotify.com/artist/46d1ndWtnYj9kxd3IlQykV?si=I09Io-dATHuIAL7TNX8Hiw"
             >
               Chill Beats
             </a>
           </span>{" "}
-          on Spotify. Also, I wasn't reading type of a person but over the time
+          on Spotify. Apart from that, I'm not a typical book-reading person but
           I have developed habit of reading books. The most recent one is{" "}
           <span>
             <a
+              target={"_blank"}
               className="underline hover:text-blue"
               href="https://www.navalmanack.com/"
             >
@@ -42,6 +46,7 @@ const About = () => {
           and currently I'm reading{" "}
           <span>
             <a
+              target={"_blank"}
               className="underline hover:text-blue"
               href="https://github.com/getify/You-Dont-Know-JS/blob/1st-ed/up%20&%20going/README.md#you-dont-know-js-up--going"
             >
@@ -53,11 +58,12 @@ const About = () => {
 
         <p className="text-xl text-gray-700 pb-9">
           I like to document things that come my way and I like to share that
-          with people. For that I write Blogs (which you can see in the
-          navigation) and I also have an Instagram Account called {"   "}
+          with people. For that I write Blogs and I also have an Instagram
+          Account called {"   "}
           <span>
             <a
               className="underline hover:text-blue"
+              target={"_blank"}
               href="https://instagram.com/day_journalising?igshid=YmMyMTA2M2Y="
             >
               day_journalising

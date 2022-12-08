1703d1b9b787ad59f4e2dbcea5a5a352dfe611db
Himali Malvawala <himalimalvawala@gmail.com>
2022-12-08 17:17:23 +0530

    fix for small and medium screens

--- components/HomePage.js
+++ components/HomePage.js
@@ -3,8 +3,8 @@ import DivContent from "./DivContent";
 const HomePage = () => {
   return (
     <div>
-      <div className="container flex flex-col md:flex md:flex-col lg:flex lg:flex-row xl:flex xl:flex-row">
-        <div className="bg-blue w-screen h-96 lg:flex-auto lg:w-6 lg:h-screen xl:flex-auto xl:w-6 xl:h-screen"></div>
+      <div className="flex flex-col lg:flex lg:flex-row justify-center items-center">
+        <div className="bg-blue w-screen h-96 lg:flex-auto lg:w-6 lg:h-screen"></div>
         <div className="bg-white w-screen h-fit lg:flex-auto lg:w-96 lg:h-screen  xl:flex-auto xl:w-96 xl:h-screen">
           <div className="ml-10 mt-80 md:ml-28 md:mt-80 xl:mt-36 xl:ml-60 md:m-auto md:w-8/12 lg:w-96 xl:w-80 ">
             <h1 className="font-black text-5xl mb-6 md:text-6xl lg:text-7xl lg:mb-10 xl:text-8xl xl:mb-10">
@@ -18,7 +18,7 @@ const HomePage = () => {
           </div>
         </div>
       </div>
-      <div className="shadow-2xl bg-light-yellow rounded-md h-fit w-5/6 absolute top-60 left-9 md:left-28 md:top-48 md:w-9/12 lg:w-3/12 lg:left-44 xl:w-3/12 xl:h-4/6 xl:left-96">
+      <div className="shadow-2xl bg-light-yellow rounded-md h-fit w-5/6 absolute top-60 left-9 sm:left-14 md:left-28 md:top-48 md:w-9/12 lg:w-3/12 lg:left-44 xl:w-3/12 xl:h-4/6 xl:left-96">
         <DivContent />
       </div>
     </div>
